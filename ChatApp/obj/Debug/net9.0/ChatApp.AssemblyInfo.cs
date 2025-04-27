875b171bc3b75cda6830e79dcef08b3ee79bf079
Mphiliseni <Mphilisenialexmhlongo@gmail.com>
2025-04-27 15:16:57 +0200

    modified chatHeader.razor

--- ChatApp/obj/Debug/net9.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net9.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db17a008a0e9663e402c22fe2e991831ade59638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4968a2ed9c9964eb959b034bfb9750dad5db9fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
